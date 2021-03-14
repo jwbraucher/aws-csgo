bd39d7fd1fdd9c49b1ab33e6ba0d7dc8206720f7
Jeff Braucher <jeff@braucher.net>
2021-03-14 13:20:37 -0600

    don't remove python2 because not there

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 # bootstrap ansible and awscli because they are needed for provisioning
 # and also so that they are available on the ami later
 sudo apt update
-sudo apt remove -y python2-setuptools python2-pip
 sudo apt install -y python3-setuptools
 sudo easy_install3 pip
 sudo pip3 install --no-cache-dir ansible awscli
